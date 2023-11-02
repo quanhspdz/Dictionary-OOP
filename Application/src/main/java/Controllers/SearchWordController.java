5a40419562a36df6799dfbf92e14d8ea3cc0316a
quanhspdz <pc.quanhdz@gmail.com>
2023-11-02 16:09:47 +0700

    Thêm tính năng tìm kiếm từ theo tên
    Thêm tính năng tìm kiếm từ theo tên

--- Application/src/main/java/Controllers/SearchWordController.java
+++ Application/src/main/java/Controllers/SearchWordController.java
@@ -1,43 +1,69 @@
 package Controllers;
 
+import App.DictionaryApp;
+import Interfaces.DataLoadedListener;
 import Models.Word;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import static App.DictionaryApp.data;
 
 public class SearchWordController implements Initializable {
-    private static final String DATA_FILE_PATH = "Application/src/main/data/E_V.txt";
-    private static final String SPLITTING_CHARACTERS = "<html>";
-    private Map<String, Word> data = new HashMap<>();
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            readData();
+        if (data.isEmpty()) {
+            DictionaryApp.addDataLoadedListener(new DataLoadedListener() {
+                @Override
+                public void onDataLoaded() {
+                    loadWordList(); // Thực hiện loadWordList() khi dữ liệu đã được tải
+                }
+            });
+        } else {
             loadWordList();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
+
+        searchTerm.setOnKeyTyped(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                String searchKey = searchTerm.getText();
+                if (searchKey.isEmpty()) {
+                    cancelBtn.setVisible(false);
+                    showDefaultListView();
+                } else {
+                    cancelBtn.setVisible(true);
+                    handleSearchOnKeyTyped(searchKey);
+                }
+            }
+        });
     }
 
     public void loadWordList() {
-        this.wordListView.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> {
-                    Word selectedWord = data.get(newValue.trim());
-                    String definition = selectedWord.getDef();
-                    this.selectedWord.setText(selectedWord.getWord());
-                    definitionWebView.getEngine().loadContent(definition, "text/html");
-                }
-        );
+        this.wordListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                // Đảm bảo rằng newValue không null
+                Word selectedWord = data.get(newValue.trim());
+                String definition = selectedWord.getDef();
+                this.selectedWord.setText(selectedWord.getWord());
+                definitionWebView.getEngine().loadContent(definition, "text/html");
+            }
+        });
+
+        showDefaultListView();
+    }
+
+    private void showDefaultListView() {
         // Chuyển danh sách từ Map thành danh sách có thứ tự
         List<String> sortedWords = new ArrayList<>(data.keySet());
 
@@ -48,16 +74,32 @@ public class SearchWordController implements Initializable {
         this.wordListView.getItems().setAll(sortedWords);
     }
 
-    public void readData() throws IOException {
-        FileReader fis = new FileReader(DATA_FILE_PATH);
-        BufferedReader br = new BufferedReader(fis);
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] parts = line.split(SPLITTING_CHARACTERS);
-            String word = parts[0];
-            String definition = SPLITTING_CHARACTERS + parts[1];
-            Word wordObj = new Word(word, definition);
-            data.put(word, wordObj);
+    private void showSearchResultListView(List<String> searchResultList) {
+        // Sắp xếp danh sách các từ theo thứ tự bảng chữ cái (alpha)
+        Collections.sort(searchResultList);
+
+        // Đặt danh sách đã sắp xếp vào wordListView
+        this.wordListView.getItems().setAll(searchResultList);
+    }
+
+    @FXML
+    private void handleSearchOnKeyTyped(String searchKey) {
+        List<String> searchResultList = new ArrayList<>();
+        // Chuyển sang chữ thường để tìm kiếm không phân biệt chữ hoa/chữ thường
+        searchKey = searchKey.toLowerCase();
+
+        for (String key : data.keySet()) {
+            if (key.toLowerCase().startsWith(searchKey)) {
+                searchResultList.add(key);
+            }
+        }
+
+        if (searchResultList.isEmpty()) {
+            notAvailableAlert.setVisible(true);
+            showDefaultListView();
+        } else {
+            notAvailableAlert.setVisible(false);
+            showSearchResultListView(searchResultList);
         }
     }
 
