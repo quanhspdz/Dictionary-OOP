bbcab7041e7b87b51ce96104feaad68ab3db5d5a
dungtt <dungtt@intern.vn>
2023-11-11 15:13:17 +0700

    rename login controller

--- Application/src/main/java/Controllers/LoginController.java
+++ Application/src/main/java/Controllers/LoginController.java
@@ -1,12 +1,10 @@
 package Controllers;
 
 import java.net.URL;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -14,22 +12,17 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 
-public class SignUpController implements Initializable {
+public class LoginController implements Initializable {
 
     @FXML
     private Button changePass_backBtn;
