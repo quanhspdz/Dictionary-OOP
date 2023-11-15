d24f5653543db2dd931c5f7d8c3303758dff3f20
quanhspdz <pc.quanhdz@gmail.com>
2023-11-15 16:53:04 +0700

    setup timer và score

--- Application/src/main/java/Controllers/LearningEngController.java
+++ Application/src/main/java/Controllers/LearningEngController.java
@@ -23,7 +23,9 @@ import javafx.util.Duration;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static Constant.Constant.*;
 
@@ -43,7 +45,7 @@ public class LearningEngController implements Initializable {
     @FXML
     private Label progressText1, progressText2, progressText3, progressText4,
             progressText5, progressText6, progressText7, progressText8, progressText9, progressText10,
-            questionText, questionTitle;
+            questionText, questionTitle, scoreLabel, countdownLabel;
 
     private Media correctAudioMedia;
     private MediaPlayer correctAudioMediaPlayer;
@@ -51,10 +53,23 @@ public class LearningEngController implements Initializable {
             correctQuestion = Color.rgb(166, 232, 58),
             incorrectQuestion = Color.rgb(255, 216, 0);
 
+    private ArrayList<Question> listQuestionData = new ArrayList<>();
     private ArrayList<Question> listQuestion = new ArrayList<>();
     private int currentQuestionIndex;
 
     private String answer = "";
+
+    private boolean isCompleted = false;
+
+    private boolean isHandledFinishAnimation = false;
+
+    private Timeline mainTimeLine = new Timeline();
+
+    private int score = 0;
+
+    Timeline countdownTimer;
+
+    private final int correctScore = 10;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -102,8 +117,56 @@ public class LearningEngController implements Initializable {
         setupQuestion();
     }
 
+    public void pickQuestions() {
+        if (listQuestionData.size() <= 10) {
+            // If the list of available questions is 10 or fewer, use all of them
+            listQuestion.addAll(listQuestionData);
+        } else {
+            // If there are more than 10 questions, pick 10 random questions
+            Random random = new Random();
+            ArrayList<Integer> selectedIndices = new ArrayList<>();
+
+            while (selectedIndices.size() < 10) {
+                int randomIndex = random.nextInt(listQuestionData.size());
+
+                // Ensure we don't pick the same question more than once
+                if (!selectedIndices.contains(randomIndex)) {
+                    selectedIndices.add(randomIndex);
+                }
+            }
+
+            // Add the randomly selected questions to listQuestion
+            for (int index : selectedIndices) {
+                listQuestion.add(listQuestionData.get(index));
+            }
+        }
+    }
+
+
+    private void nextQuestion() {
+        if (currentQuestionIndex + 1 < listQuestion.size()) {
+            currentQuestionIndex++;
+            Question question = listQuestion.get(currentQuestionIndex);
+            showQuestion(question);
+            questionTitle.setText("Câu hỏi " + (currentQuestionIndex + 1) + ":");
+        } else if (currentQuestionIndex + 1 >= listQuestion.size()) {
+            //Disable buttons
+            answerBtnA.setDisable(true);
+            answerBtnB.setDisable(true);
+            answerBtnC.setDisable(true);
+            answerBtnD.setDisable(true);
+
+            isCompleted = true;
+            nextBtn.setText("Hoàn thành");
+        }
+    }
+
     private void checkAnswer() {
         Question question = listQuestion.get(currentQuestionIndex);
+        if (answer.isEmpty()) {
+            return;
+        }
+        countdownTimer.stop();
         if (answer.equals(question.getCorrectAnswer())) {
             handleCorrectAnswer();
         } else {
@@ -112,17 +175,26 @@ public class LearningEngController implements Initializable {
     }
 
     private void handleIncorrectAnswer() {
+        setQuestionProgress(currentQuestionIndex + 1, incorrectQuestion);
         createAnimation(questionAndAnswerPane, false);
     }
 
     private void handleCorrectAnswer() {
+        setQuestionProgress(currentQuestionIndex + 1, correctQuestion);
+        updateScore(correctScore);
         createAnimation(questionAndAnswerPane, true);
     }
 
+    private void updateScore(int correctScore) {
+        score += correctScore;
+        scoreLabel.setText(score + "/100");
+    }
+
     private void setupQuestion() {
-        Question.readQuestionFile(listQuestion);
-        currentQuestionIndex = 1;
-        questionTitle.setText("Câu hỏi " + currentQuestionIndex + ":");
+        Question.readQuestionFile(listQuestionData);
+        pickQuestions();
+        currentQuestionIndex = 0;
+        questionTitle.setText("Câu hỏi " + (currentQuestionIndex + 1) + ":");
         showQuestion(listQuestion.get(0));
     }
 
@@ -132,6 +204,33 @@ public class LearningEngController implements Initializable {
         answerBtnB.setText(question.getAnswerB());
         answerBtnC.setText(question.getAnswerC());
         answerBtnD.setText(question.getAnswerD());
+        setQuestionProgress(currentQuestionIndex + 1, currentQuestion);
+        setupCountdownTimer();
+    }
+
+    private void setupCountdownTimer() {
+        AtomicInteger secondsRemaining = new AtomicInteger(30);
+        countdownTimer = new Timeline(new KeyFrame(Duration.seconds(1), (ActionEvent event) -> {
+            secondsRemaining.getAndDecrement();
+            countdownLabel.setText(String.valueOf(secondsRemaining.get()) + "s");
+
+            if (secondsRemaining.get() <= 0) {
+                countdownTimer.stop();
+                countdownLabel.setText("Time's up!");
+                handleTimeUp();
+            }
+        }));
+
+        countdownTimer.setCycleCount(Timeline.INDEFINITE);
+        countdownTimer.play();
+    }
+
+    private void handleTimeUp() {
+        if (answer.isEmpty()) {
+            handleIncorrectAnswer();
+        } else {
+            checkAnswer();
+        }
     }
 
     private void createAnimation(Pane container, Boolean isCorrect) {
@@ -159,6 +258,8 @@ public class LearningEngController implements Initializable {
         int numFireworks = 30;
 
         for (int i = 0; i < numFireworks; i++) {
+            Timeline timeline = new Timeline();
+
             // Tạo một ImageView với hình ảnh pháo hoa
             ImageView firework = new ImageView(originalImage);
 
@@ -174,9 +275,6 @@ public class LearningEngController implements Initializable {
 
             // Thêm hạt pháo hoa vào container
             container.getChildren().add(firework);
-
-            // Tạo animation cho hạt pháo hoa
-            Timeline timeline = new Timeline();
 
             // KeyValues cho lắc lắc
             KeyValue keyValueScaleX = new KeyValue(firework.scaleXProperty(), 2); // Kích thước X
@@ -199,14 +297,6 @@ public class LearningEngController implements Initializable {
             /// Xóa hạt pháo hoa sau khi hoàn thành animation
             timeline.setOnFinished(event -> {
                 container.getChildren().remove(firework);
-                correctAudioMediaPlayer.stop(); // Dừng phát âm thanh sau khi hiệu ứng hoàn thành
-
-                //Enable buttons
-                answerBtnA.setDisable(false);
-                answerBtnB.setDisable(false);
-                answerBtnC.setDisable(false);
-                answerBtnD.setDisable(false);
-                nextBtn.setDisable(false);
             });
 
             //Disable buttons
@@ -218,7 +308,34 @@ public class LearningEngController implements Initializable {
 
             // Bắt đầu animation
             timeline.play();
+
+            if (i == 0) {
+                mainTimeLine = timeline;
+            }
         }
+
+        mainTimeLine.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                correctAudioMediaPlayer.stop(); // Dừng phát âm thanh sau khi hiệu ứng hoàn thành
+
+                //Enable buttons
+                answerBtnA.setDisable(false);
+                answerBtnB.setDisable(false);
+                answerBtnC.setDisable(false);
+                answerBtnD.setDisable(false);
+                nextBtn.setDisable(false);
+
+                answerBtnA.setSelected(false);
+                answerBtnB.setSelected(false);
+                answerBtnC.setSelected(false);
+                answerBtnD.setSelected(false);
+
+                nextQuestion();
+                answer = "";
+                isHandledFinishAnimation = true;
+            }
+        });
     }
 
     private void setQuestionProgress(int questionNumber, Paint color) {
